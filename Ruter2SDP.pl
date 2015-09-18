1613b90540c52db73e11290a51b24cec81240827
eier7 <eivind.ervik@gmail.com>
2015-09-18 12:04:05 +0000

    utm + div

---
+++ Ruter2SDP.pl
@@ -0,0 +1,232 @@
+#!/usr/bin/perl
+# ---------------------------------------------------------------------------
+# Ruter2SDP.pl
+#
+# Generates Route data to feed into Dynamic Positioning system SDP10
+#
+# Usage: Ruter2SDP [--layer=1[,2,3..10]] file(s)...
+#
+# Input: Olex Ruter format
+# Output: <STDOUT>
+#
+# ---------------------------------------------------------------------------
+# By: A. Lagestrand, al@blom.no                      20(c)03 Blom Maritime AS
+# ---------------------------------------------------------------------------
+
+use File::Basename;
+use Time::Local;
+use Math::Trig;
+use Math::Trig 'great_circle_direction';
+use Math::Trig 'great_circle_distance';
+
+# --  Variables  ------------------------------------------------------------
+$script    = basename($0,".pl");
+
+$rutetype  = "RUTE";
+$lineno    = 0;
+$DATUM     = "WGS84";	# always WGS84 in Olex Chart System
+
+($file,@files) = ();
+(@lat,@lon) = ();
+@plottsett  = ();
+
+
+# ---------------------------------------------------------------------------
+
+&decodeArgs();
+
+&msg("**  Reading Olex 'Ruter'  **");
+&DPheader();
+
+foreach $file (@files) {
+  &msg("- reading $file...");
+  open(RUTER,$file);
+
+  while (<RUTER>) {
+    $_ =~ s/(\s+)/ /g; chop();
+
+    if (/^Rute (.*)/) {	# NEW LINE
+      &flushdata($rutenavn,$rutetype) if ( &checkInArray($plottsett,@plottsett) );
+      $lineno++;
+      if (! $fast_rutenavn) {
+        $rutenavn = sprintf("%s %d",$1,$lineno);
+      } else {
+        $rutenavn = sprintf("%s %d",$fast_rutenavn,$lineno);
+      }
+      (@lat,@lon,@navn) = ();
+      next;
+    }
+
+    #if (/^Linjefarge (.*)$/) { $farge       = $1;     next; }	# linecolor
+    if (/^Rutetype (.*)$/)   { $rutetype    = uc($1); next; }	# elementtype
+    if (/^Plottsett (.*)$/)  { $plottsett   = $1;     next; }	# plotlayer
+    if (/^Navn (.*)$/)       { $navn[$#lat] = "$1";   next; }	# waypoint name
+
+    # DECODE COORDINATES
+    if (/^(\d+\.?\d*) (\d+\.?\d*) (\d+) (.*)$/) { # lat,lon,time,symbol
+    	$lat = $1 / 60;			# decimal degrees
+    	$lon = $2 / 60;			# decimal degrees
+	#print "## $lat - $lon\n";
+	push @lat,$lat;
+	push @lon,$lon;
+    }
+  }
+  
+  close RUTER;
+}
+
+# Flush last rutetype in file
+if ( &checkInArray($plottsett,@plottsett) ){
+  &flushdata($rutenavn,$rutetype);
+}
+
+&DPfooter;
+
+exit 0;
+
+# --  SUB ROUTINES  ---------------------------------------------------------
+
+sub err { foreach (@_) { print STDERR "\n$_ \n\n"; } exit 1; }
+sub msg {
+  foreach (@_) {
+    if ($_) {
+      print STDERR "**  $_  **\n";
+    } else {
+      print STDERR "\n";
+    }
+  }
+}
+
+sub usage {
+  print  STDERR "\nusage: $script [--layer=1[,2,3..10]] file(s)...\n\n";
+  printf STDERR "    %s\n","This script takes an Olex 'Ruter' file, and generates";
+  printf STDERR "    %s\n","an output for import into SDP 10 Dynamic Positioning System.";
+  printf STDERR "    %s\n","All positions are WGS84 - latitude/longitude.\n\n";
+  exit;
+}
+
+sub decodeArgs {
+  my (@layers,$layer) = ();
+  
+  if (! @ARGV) { &msg("","Missing argument(s)"); usage(); }
+
+  foreach (@ARGV){
+	if (/^-h|--help$/) {
+		&usage();
+	} elsif (/^--layer=(.*)$/) {
+		@layers = split(",|;",$1);
+		foreach $layer (@layers){
+		  $layer--;
+		  push (@plottsett,2**$layer);
+		}
+	} elsif (/^--name=(.*)$/) {
+		$fast_rutenavn = $1;
+	} else {
+		if (-f $_){ push (@files,$_); }	# input file
+	}
+  }
+  
+  if (! @files) { &err("No input file(s)...!"); }
+}
+
+sub flushdata {
+  my $rutenavn	= shift;
+  my $rutetype	= shift;
+  my $i		= ();
+  
+  my @header = qw(Format Id HemisNS LatDeg LatMin HemisEW LonDeg LonMin LegType Head Speed TurnRad);
+  my %element	= {};
+
+ 
+  if (! @lat) { return; }			# empty element
+  if ($#lat < 2) {				# single point
+    &msg("Less than 2 positions in element (single point)","Not supported - skipped!");
+    return;
+  }
+  if ($rutetype =~ /^AREAL$/) {			# closed polygon
+    &msg("Areal not supported - element skipped!");
+    return;
+  }
+
+  # write element header
+  printf "Trackname,%s\n",$rutenavn;
+  printf "NoOfWp,%d\n",   $#lat+1;
+  printf "Datum,%s\n",    $DATUM;
+  print  "WP$header[0]";
+  foreach (1 .. $#header) { printf ",WP%s",$header[$_]; }
+  print  "\n";
+  
+  
+  foreach $i (0 .. $#lat) {
+    $element{LegType} = 0;
+    #$element{LegDist} = &calcDistance($lat[$i],$lon[$i],$lat[$i+1],$lon[$i+1]);
+    #$element{Head}    = &calcBearing($lat[$i],$lon[$i],$lat[$i+1],$lon[$i+1]);
+    $element{LegDist} = 0;
+    $element{Head}    = 0;
+    $element{Speed}   = 0;
+    $element{TurnRad} = 0;
+    
+    printf "WP,%d,%s,%02d,%08.5f,%s,%02d,%08.5f",$i+1,&deg2min('LAT',$lat[$i]),&deg2min('LON',$lon[$i]); # ID + POSITION
+    printf ",%d",$element{LegType};
+    printf ",%.3f",$element{Head};
+    printf ",%.4f",$element{Speed};
+    printf ",%.2f",$element{TurnRad};
+    print  "\n";
+  }
+}
+
+sub calcDistance {
+  my ($lat1,$lon1,$lat2,$lon2) = @_;
+  my $rho = 6371;		# earth radius
+  my $b = great_circle_distance(deg2rad($lon1),pi/2 - deg2rad($lat1),deg2rad($lon2),pi/2 - deg2rad($lat2), $rho);
+  
+  if ((! $lat2) and (! $lon2)) { return(); }
+  return($b);
+}
+
+sub calcBearing {
+  my ($lat1,$lon1,$lat2,$lon2) = @_;
+  my $lat1 = deg2rad($_[0]);
+  my $lon1 = deg2rad($_[1]);
+  my $lat2 = deg2rad($_[2]);
+  my $lon2 = deg2rad($_[3]);
+  my $dlong = $lon2 - $lon1;
+  my $b = atan2( sin($dlong)*cos($lat2) , cos($lat1)*sin($lat2) - sin($lat1)*cos($lat2)*cos($dlong) );
+  #my $b = great_circle_direction($lon1,$lat1,$lon2,$lat2); # From Math::Trig - not working
+
+  if ((! $lat2) and (! $lon2)) { return(); }
+  if ($b < 0) {
+    $b = rad2deg($b) + 360;
+  } else {
+    $b = rad2deg($b);
+  }
+  return($b);
+}
+
+sub deg2min {
+  my $type = shift;
+  my $deg  = int($_[0]);
+  my $min  = ($_[0] - $deg) * 60;
+
+  if ($type =~/^LAT$/){					# latitude
+    if ($deg < 0){ $type = 'S'} else { $type = 'N'; }
+  } elsif ($type =~/^LON$/) {				# longitude
+    if ($deg < 0){ $type = 'W'} else { $type = 'E'; }
+  }
+  return($type,$deg,$min);
+}
+
+sub checkInArray {
+  my $value = shift;
+  if ( ! @_ ) { return(1); }
+  foreach (@_){ if ( $value == $_ ) { return(1)}; }
+  return(0);
+}
+
+sub DPfooter { print "END\n"; }
+sub DPheader {
+  my $Version = 4;
+  my @time = localtime();
+  printf "%04d-%02d:%02d\n",$time[5]+1900,$time[2],$time[1];
+  printf "Version,%d\n",$Version;
+}
--- olextodms.py
+++ olextodms.py
@@ -4,6 +4,7 @@
 import os
 import re
 import sys
+import utm
 import math
 import time
 import gzip
@@ -16,10 +17,10 @@ libc = ctypes.CDLL("libc.so.6")
 powerled = 17
 busyled = 27
 
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(powerled, GPIO.OUT)
 GPIO.setup(busyled, GPIO.OUT)
-
 GPIO.output(powerled, 1)
 
 found = True
@@ -47,49 +48,66 @@ while(True):
                     if os.path.realpath(path).find("/usb") > 0:
                         if not len(devices) > 0:
                             GPIO.output(busyled, 1)
+                            GPIO.output(powerled, 0)
                             print("FANT USB-ENHET")
                             devices.append(deviceName)
                             p = subprocess.Popen(["mount", "-t", "auto", "/dev/%s" % deviceName + "1", "/mnt/usb"])
                             p.communicate()
                             libc.sync()
-                            if not os.path.exists("/mnt/usb/dms"):
-                                os.makedirs("/mnt/usb/dms")
+                            if not os.path.exists("/mnt/usb/utm"):
+                                os.makedirs("/mnt/usb/utm")
+                            if not os.path.exists("/mnt/usb/dp"):
+                                os.makedirs("/mnt/usb/dp")
                             for rutefil in os.listdir("/mnt/usb"):
                                 if(rutefil.endswith(".gz")):
                                     try:
                                         f = gzip.open("/mnt/usb/%s" % rutefil) 
-                                        o = open("/mnt/usb/dms/%s" % os.path.splitext(rutefil)[0] + ".csv", "w+", encoding="latin-1")
+                                        o = open("/mnt/usb/utm/%s" % os.path.splitext(rutefil)[0] + ".csv", "w+", encoding="latin-1")
+                                        odp = open("/mnt/usb/%s" % os.path.splitext(rutefil)[0] + ".tmp", "w+", encoding="latin-1")
                                         for line in f:
                                             line = line.decode("utf-8")
+                                            odp.write(line)
                                             m = re.match("^(\d*\.\d*) (\d*\.\d*)", line)
                                             if(m):
                                                 lat = float(m.group(1)) / 60
                                                 lon = float(m.group(2)) / 60
-                                                if(lat > 0): latd = 'N'
-                                                else:        latd = 'S'
-                                                if(lon > 0): lond = 'E'
-                                                else:        lond = 'W'
-                                                Dlat = str(math.floor(lat))
-                                                Mlat = str(math.floor(abs(lat) * 60) % 60)
-                                                Slat = (abs(lat) * 3600) % 60
-                                                Slat = str("%.4f" % Slat)
-                                                Dlon = str(math.floor(lon))
-                                                Mlon = str(math.floor(abs(lon) * 60) % 60)
-                                                Slon = (abs(lon) * 3600) % 60
-                                                Slon = str("%.4f" % Slon)
-                                                o.write(Dlat+ u"\N{DEGREE SIGN}" + Mlat + "\'" + Slat +"\"" + latd + "," +  Dlon + u"\N{DEGREE SIGN}" + Mlon + "\'" + Slon +"\"" + lond + "\r\n")
+                                                utmd = utm.from_latlon(lat,lon)
+                                                o.write("%0.2f" % utmd[0] + "," + "%0.2f" % utmd[1] + ","  + str(utmd[2]) + "," + utmd[3] + "\r\n")
                                                 libc.sync()
-                                                
+#                                        if(lat > 0): latd = 'N'
+#                                        else:        latd = 'S'
+#                                        if(lon > 0): lond = 'E'
+#                                        else:        lond = 'W'
+#                                        Dlat = str(math.floor(lat))
+#                                        Mlat = str(math.floor(abs(lat) * 60) % 60)
+#                                        Slat = (abs(lat) * 3600) % 60
+#                                        Slat = str("%.4f" % Slat)
+#                                        Dlon = str(math.floor(lon))
+#                                        Mlon = str(math.floor(abs(lon) * 60) % 60)
+#                                        Slon = (abs(lon) * 3600) % 60
+#                                        Slon = str("%.4f" % Slon)
+#                                        o.write(Dlat+ u"\N{DEGREE SIGN}" + Mlat + "\'" + Slat +"\"" + latd + "," +  Dlon + u"\N{DEGREE SIGN}" + Mlon + "\'" + Slon +"\"" + lond + "\r\n")
+                                        odp.close()         
+                                        proc = subprocess.Popen(["/home/pi/olextodms/Ruter2SDP.pl", "/mnt/usb/%s" % os.path.splitext(rutefil)[0] + ".tmp"], stdout=subprocess.PIPE)
+                                        (out, err) = proc.communicate()
+                                        fdp = open("/mnt/usb/dp/%s" % os.path.splitext(rutefil)[0] + ".txt", "w+")
+                                        fdp.write(out.decode("latin-1"))
+                                        fdp.close()
+                                        GPIO.output(powerled, 1)
+                                        time.sleep(.1)
+                                        GPIO.output(powerled, 0)
                                     except:
                                         print("FILE ERROR")
-                            #time.sleep(5)
                             o.close()
                             f.close()
                             p = subprocess.Popen(["umount", "/mnt/usb"])
                             p.communicate()
                             print("KONVERTERING FERDIG")
                             GPIO.output(busyled, 0)
+                            GPIO.output(powerled, 1)
 
     except:
         print("ERROR")
+        GPIO.output(powerled, 1)
+        GPIO.output(busyled, 0)
     time.sleep(1)
